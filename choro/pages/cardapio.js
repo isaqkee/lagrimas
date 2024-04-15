3a3ddfc137df8290761dbc780a2749e52b0f43b6
isaqkee <141652864+isaqkee@users.noreply.github.com>
2024-04-15 14:44:14 -0300

    full terminado

--- choro/pages/cardapio.js
+++ choro/pages/cardapio.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Text} from "react-native";
+import { View, StyleSheet, Text, Image} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export function Dois() {
@@ -8,7 +8,18 @@ export function Dois() {
                 <Text style={ESTILO.title}>
                     Cardápio - Isaqkeizin
                 </Text>
-            </View>
+                 </View>
+                <View>
+                    <Image style={ESTILO.image} source={require('../src/img/coxinha.jpg')}></Image>
+                    <Text>
+                        Coxinha - $2.00
+                    </Text>
+                    <Image style={ESTILO.image} source={require('../src/img/paodequeijo.jpg')}></Image>
+                    <Text>
+                        Pão de Quejio - $1.50
+                    </Text>
+                </View>
+           
         </SafeAreaView>
     )
 }
@@ -23,6 +34,12 @@ const ESTILO = StyleSheet.create({
         fontSize: 18,
         fontWeight: "bold",
         color: "#FFF"
+    },
+    image: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 412,
+        height: 240
     }
 
 })
